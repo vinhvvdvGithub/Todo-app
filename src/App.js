480e97badf51fcb4464d7ac07254287a6293c9e8
vinhvvdvGithub <vinhvvdvGithub@github.com>
2021-10-05 20:33:39 +0700

    add item, show list item

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import Textfield from "@atlaskit/textfield"
+import Button from '@atlaskit/button';
+import TodoList from './component/TodoList';
+import { useState,useCallback } from "react";
+import { v4 } from "uuid";
+
 
 function App() {
+  const [todoList,setTodoList] = useState([]);
+  const [textInput,setTextInput] = useState("");
+
+  const onTextInputChange = useCallback(
+    (e) => {
+      setTextInput(e.target.value);
+    },[])
+
+  const onAddBtnClick =useCallback(
+    (e)=>{
+      setTodoList([
+        {id:v4(),name:textInput,isCompleted:false},
+        ...todoList,     
+      ]);
+      setTextInput("");
+    },
+    [textInput,todoList]
+  );
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <h3>Danh sach can lam</h3>
+    <Textfield
+      name="add-todo"
+      placehoder="Them viec can lam..."
+      elemAfterInput={
+        <Button isDisabled={!textInput} appearance='primary' onClick={onAddBtnClick}>
+          Add 
+        </Button>
+      }
+      css={{padding: '2px 4px 2px'}}
+      value={textInput}
+      onChange={onTextInputChange}
+    ></Textfield>
+    <TodoList todoList={todoList}/>
+    </>
   );
 }
 
